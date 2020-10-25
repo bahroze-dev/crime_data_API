597843fcac0476fafc3048ce12456e6bf4d2f4c8
hawkeye <jattali12@gmail.com>
2020-10-26 02:42:34 +0500

    added scenario one

--- app.py
+++ app.py
@@ -45,6 +45,13 @@ with open(Pkl_Filename, 'rb') as file:
     Pickle_KNN_CRIME_CLASS_Scaler = pickle.load(file)
 
 ##
+## importing recomendation matrix of data
+import numpy
+dict_data = numpy.load('./models/cosine_matrix.npz')
+# extract the first array
+cosine_sim = dict_data['arr_0']
+
+##
 
 
 @app.route("/", methods=["GET"])
@@ -60,8 +67,8 @@ def get_crime_data():
         data = data_santized
     ) 
 
-@app.route("/get_scenario2_data", methods=["GET"])
-def get_scenario2_data():
+@app.route("/get_scenarios_data", methods=["GET"])
+def get_scenarios_data():
     all_offenses = defaultDB.db.crime.distinct("OFNS_DESC")
     all_genders = defaultDB.db.crime.distinct("PERP_SEX")
     all_Local_Area = defaultDB.db.crime.distinct("Local Area")
@@ -89,11 +96,109 @@ def get_scenario2_predicted():
     ) 
 
 
+## recommendation of suspects
+
+@app.route("/get_recommended_suspects", methods=["GET"])
+def get_recommended_suspects():
+
+    get_suspects_recommended = defaultDB.db.predicted_suspects.find({}).sort('Time',-1)
+    
+    data_santized_recommended = json.loads(json_util.dumps(get_suspects_recommended))
+    
+    return jsonify(
+        recommended_suspects = data_santized_recommended[0], 
+    ) 
+
+
+
+
+
+
+#scenario one predicted
+@app.route("/recommendsSuspectList", methods=["POST"])
+def recommendsSuspectList():
+    req_data = request.get_json()
+    
+    crime_type = req_data['crimeType']
+    location = req_data['location']
+    
+    
+    location_cat = defaultDB.db.crime.find_one({'Local Area':location},{'Local_Area_Cat':1})
+    location_cat = location_cat['Local_Area_Cat']
+    
+
+    suspects_list = defaultDB.db.crime.find(
+        {'OFNS_DESC':crime_type,"Local_Area_Cat":location_cat},
+        {"index":1,"Name":1,"Age":1,"PERP_RACE":1,"PERP_SEX":1,"_id":0}
+    )
+
+    data_santized_suspects_list = json.loads(json_util.dumps(suspects_list))
+
+
+    recommendation_list = []
+    for docs in data_santized_suspects_list:
+        name = docs["Name"]
+        index = docs["index"]
+        recommendation_list.append(docs)
+        recommendation_suspects = get_recommendations(name,cosine_sim,index)
+        for i in recommendation_suspects:
+            document_suspect = defaultDB.db.crime.find_one({'index':i},{"Name":1,"Age":1,"PERP_RACE":1,"PERP_SEX":1,"_id":0})
+            recommendation_list.append(document_suspect)
+    
+    ## This scenario is considered if the crime has never been commited in this region so we take crime only as input
+    ## and check features of similiar suspects with everyone else and top are listed
+    if not recommendation_list:
+        suspects_list = defaultDB.db.crime.find(
+            {'OFNS_DESC':crime_type},
+            {"index":1,"Name":1,"Age":1,"PERP_RACE":1,"PERP_SEX":1,"_id":0}
+        )
+        data_santized_suspects_list = json.loads(json_util.dumps(suspects_list))
+        for docs in data_santized_suspects_list:
+            name = docs["Name"]
+            index = docs["index"]
+            recommendation_list.append(docs)
+            recommendation_suspects = get_recommendations(name,cosine_sim,index)
+            for i in recommendation_suspects:
+                document_suspect = defaultDB.db.crime.find_one({'index':i},{"Name":1,"Age":1,"PERP_RACE":1,"PERP_SEX":1,"_id":0})
+                recommendation_list.append(document_suspect)
+        
+
+
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    
+    mydict = { "recommended_suspects":recommendation_list ,"Time": dt_string }
+    x = defaultDB.db.predicted_suspects.insert_one(mydict)
+
+
+   
+    return jsonify(
+        msg = "success", 
+    ) 
+
+def get_recommendations(name, cosine_sim=cosine_sim, index=0):
+    # Get the index of the suspects that matches the name
+    idx = int(index)
+
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:3]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+    
+    # Return the top 10 most similar movies
+    return movie_indices
+
 
 
 @app.route("/predictSuspectType", methods=["POST"])
 def predict_suspect_type():
-    print("herer")
     req_data = request.get_json()
     
     crime_type = req_data['crimeType']
